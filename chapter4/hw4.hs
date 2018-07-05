2938c0f3ffb5d451a08ad37d9684eda3e12b4a13
Song Kun <songkun.fp@gmail.com>
2018-07-05 23:38:53 +0800

    finish hw4

--- chapter4/hw4.hs
+++ chapter4/hw4.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -Wall #-}
 module HW04 where
@@ -33,26 +34,40 @@ instance (Num a, Eq a, Show a) => Show (Poly a) where
                           f (1, 1)  = "x"
                           f (1, -1) = "-x"
                           f (e, c)
+                            | c == 1     = "x^" ++ show e
+                            | c == -1    = "-x^" ++ show e
                             | abs e == 1 = show c ++ "x"
                             | otherwise  = show c ++ "x^" ++ show e
 
 -- Exercise 4 -----------------------------------------
 
 plus :: Num a => Poly a -> Poly a -> Poly a
-plus = undefined
+plus (P p) (P q) = P (g p q)
+  where g [] [] = []
+        g [] ys = ys
+        g xs [] = xs
+        g (x : xs) (y : ys) = x + y : g xs ys
 
 -- Exercise 5 -----------------------------------------
 
 times :: Num a => Poly a -> Poly a -> Poly a
-times = undefined
+times (P p) (P q) = foldr plus (P [0]) $ map (P) $ t (zip [0 ..] p) q
+  where t [] _ = []
+        t _ [] = []
+        t ((i, x) : xs) ys = (shift i $ map (* x) ys) : t xs ys
+
+shift :: (Num a) => Int -> [a] -> [a]
+shift 0 xs = xs
+shift n xs = 0 : shift (n - 1) xs
+
 
 -- Exercise 6 -----------------------------------------
 
 instance Num a => Num (Poly a) where
     (+) = plus
     (*) = times
-    negate      = undefined
-    fromInteger = undefined
+    negate (P xs) = P $ map (* (-1)) xs
+    fromInteger n = P [fromInteger n]
     -- No meaningful definitions exist
     abs    = undefined
     signum = undefined
@@ -60,17 +75,20 @@ instance Num a => Num (Poly a) where
 -- Exercise 7 -----------------------------------------
 
 applyP :: Num a => Poly a -> a -> a
-applyP = undefined
+applyP (P xs) n = sum (zipWith (*) (map (\(i, v) -> v^i) $ zip [0 ..] (map (* n) $ repeat 1)) xs)
 
 -- Exercise 8 -----------------------------------------
 
 class Num a => Differentiable a where
     deriv  :: a -> a
     nderiv :: Int -> a -> a
-    nderiv = undefined
+    nderiv 0 f = f
+    nderiv n f = deriv $ nderiv (n - 1) f
 
 -- Exercise 9 -----------------------------------------
 
 instance Num a => Differentiable (Poly a) where
-    deriv = undefined
+    deriv (P [])  = P []
+    deriv (P [_]) = P []
+    deriv (P cs)  = P $ tail $ map (\(i, c) -> c * (fromInteger i)) $ zip [0 ..] cs
 
